f10b2c96ceba40ac9e893bb04f1d5dd6f5bf4f10
Neerajkumarc <chaudharyneeraj91@gmail.com>
2023-08-16 20:52:16 +0530

    socket setup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
-const PORT = 8000
-const express = require("express")
-const app = express()
+const PORT = process.env.PORT || 8000
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 app.use(express.static("public"))
-app.listen(PORT,()=>{
-    console.log(`Server started on ${PORT}`);
-})
+
+io.on('connection', (socket) => {
+    io.emit("userConnected", socket.id)
+    console.log("user connected", socket.id);
+
+
+    socket.on("disconnect", () => {
+        io.emit("userDisconnected", socket.id);
+        console.log("user disconnected", socket.id);
+    })
+});
+
+server.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
+});
