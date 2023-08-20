e603b3ce7dac3516dfc9f05a611426be61e66551
Neerajkumarc <chaudharyneeraj91@gmail.com>
2023-08-20 21:52:44 +0530

    player movement

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 });
 app.use(express.static("public"))
 
 players = {}
-
+const SPEED = 30
 io.on('connection', (socket) => {
     console.log("user connected", socket.id);
     players[socket.id] = {
@@ -21,12 +21,34 @@ io.on('connection', (socket) => {
     }
     io.emit("players", players)
     console.log(players)
+
+    socket.on("keydown", (key)=>{
+        switch(key){
+            case "w":
+                players[socket.id].y -= SPEED
+                break;
+            case "a":
+                players[socket.id].x -= SPEED
+                break;
+            case "s":
+                players[socket.id].y += SPEED
+                break;
+            case "d":
+                players[socket.id].x += SPEED
+                break;
+        }
+    })
+
     socket.on("disconnect", () => {
         console.log("user disconnected", socket.id);
         delete players[socket.id]
         io.emit("players", players)
     })
 });
+
+setInterval(()=>{
+    io.emit("players", players)
+},15)
 
 server.listen(PORT, () => {
     console.log(`listening on *:${PORT}`);
